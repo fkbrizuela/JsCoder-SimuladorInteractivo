d288594a8f3e31a2b5f6acdb9c8ebec76c7f9d36
Franco Brizuela <fkbrizuela91@gmail.com>
2022-07-11 12:23:53 -0300

    condicionales de no negatividad

--- js/main.js
+++ js/main.js
@@ -91,10 +91,84 @@ function agregar(opcion2) {
             productoD = productoD + newCantidad 
             break
         default:
-            alert("or favor ingrese una opción correcta")
+            alert("Por favor ingrese una opción correcta")
             break
     }
+}
 
+function quitar(opcion3) {
+
+    let control = 0
+
+    switch (opcion3) {
+        case '1':
+            if(productoA == 0){
+                alert("No se puede sacar ninguna cantidad debido a que usted no posee nada de este producto")
+                break
+            }
+            alert(`Usted tiene una cantidad de ${productoA} del producto seleccionado, cuantas desea sacar`)
+            newCantidad = parseInt(prompt("ingrese canditdad"))
+            control = productoA
+            productoA =  productoA - newCantidad 
+            while(productoA <= -1){
+                alert(`No puede quitar a cantidad ingresada, no se puede sacar más de ${control}`)
+                productoA =  control
+                newCantidad = parseInt(prompt("por favor vuelva a ingresar la canditdad que desea sacar"))
+                productoA =  productoA - newCantidad 
+            }
+            break
+        case '2':
+            if(productoB == 0){
+                alert("No se puede sacar ninguna cantidad debido a que usted no posee nada de este producto")
+                break
+            }
+            alert(`Usted tiene una cantidad de ${productoB} del producto seleccionado, cuantas desea sacar`)
+            newCantidad = parseInt(prompt("ingrese canditdad"))
+            control = productoB
+            productoA =  productoB - newCantidad 
+            while(productoB <= -1){
+                alert(`No puede quitar a cantidad ingresada, no se puede sacar más de ${control}`)
+                productoB =  control
+                newCantidad = parseInt(prompt("por favor vuelva a ingresar la canditdad que desea sacar"))
+                productoB =  productoB - newCantidad 
+            }
+            break
+        case '3':
+            if(productoC == 0){
+                alert("No se puede sacar ninguna cantidad debido a que usted no posee nada de este producto")
+                break
+            }
+            alert(`Usted tiene una cantidad de ${productoC} del producto seleccionado, cuantas desea sacar`)
+            newCantidad = parseInt(prompt("ingrese canditdad"))
+            control = productoC
+            productoC =  productoC - newCantidad 
+            while(productoC <= -1){
+                alert(`No puede quitar a cantidad ingresada, no se puede sacar más de ${control}`)
+                productoC =  control
+                newCantidad = parseInt(prompt("por favor vuelva a ingresar la canditdad que desea sacar"))
+                productoC =  productoC - newCantidad 
+            }
+            break
+        case '4':
+            if(productoD == 0){
+                alert("No se puede sacar ninguna cantidad debido a que usted no posee nada de este producto")
+                break
+            }
+            alert(`Usted tiene una cantidad de ${productoD} del producto seleccionado, cuantas desea sacar`)
+            newCantidad = parseInt(prompt("ingrese canditdad"))
+            control = productoD
+            productoD =  productoD - newCantidad 
+            while(productoD <= -1){
+                alert(`No puede quitar a cantidad ingresada, no se puede sacar más de ${control}`)
+                productoD =  control
+                newCantidad = parseInt(prompt("por favor vuelva a ingresar la canditdad que desea sacar"))
+                productoD =  productoD - newCantidad 
+            }
+            break
+        default:
+            alert("Por favor ingrese una opción correcta")
+            break
+    }
 }
 
 alert("Si desea finalizar su compra y calcular el costo total elija la opcion 1\nSi desea agregar más productos elija la opción 2\nSi desea eliminar algún producto elija la opción 3")
@@ -117,11 +191,15 @@ switch (opcion) {
         const mostrarPrecioDos = costoTotal()
         alert(`El precio total es de $${mostrarPrecioDos}`)
         console.log(mostrarPrecioDos)
-
         break
 
     case '3':
-
+        alert("Que producto desea quitar")
+        let opcion3 = (prompt("Elija una opcion\n1_Cerveza\n2_Vino\n3_Pan\n4_Carne"))
+        quitar(opcion3)
+        const mostrarPrecioTres = costoTotal()
+        alert(`El precio total es de $${mostrarPrecioTres}`)
+        console.log(mostrarPrecioTres)
         break
 
     default:
